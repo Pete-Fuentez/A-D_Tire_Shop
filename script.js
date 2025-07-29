ad3453b40003e307d511147bdf72a6d9af4686fa
Pedro Fuentez <pedro.fuentez@okstate.edu>
2025-07-29 06:33:46 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Toggle mobile menu
+document.getElementById('menu-toggle').addEventListener('click', function () {
+    document.querySelector('.navbar ul').classList.toggle('active');
+  });
+  
